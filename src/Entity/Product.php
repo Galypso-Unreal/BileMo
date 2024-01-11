b81681cd383fa67d4b23cfa0f81c5fcbae411904
unknown <johann.moser0681@gmail.com>
2024-01-11 11:29:34 +0100

    ADD [Product] add product create test for validator and add validator system init

--- src/Entity/Product.php
+++ src/Entity/Product.php
@@ -4,6 +4,7 @@ namespace App\Entity;
 
 use App\Repository\ProductRepository;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
 
 #[ORM\Entity(repositoryClass: ProductRepository::class)]
 class Product
@@ -14,6 +15,8 @@ class Product
     private ?int $id = null;
 
     #[ORM\Column(length: 255)]
+    #[Assert\NotBlank(message: "The name of product is required")]
+    #[Assert\Length(min: 1, max: 255, minMessage: "The name of product need to be less {{ limit }} characters", maxMessage: "The title cannot be longer than {{ limit }} characters.")]
     private ?string $name = null;
 
     public function getId(): ?int
