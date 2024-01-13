65dc619636f0fb43ec6f6ef2bb973e0a087bcd10
Galypso-Unreal <johann.moser0681@gmail.com>
2024-01-13 11:49:55 +0100

    UPD [jwt] create errors and token user

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -5,7 +5,6 @@ namespace App\Controller;
 use App\Entity\Product;
 use App\Repository\ProductRepository;
 use Doctrine\ORM\EntityManagerInterface;
-use Exception;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
@@ -15,53 +14,56 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpKernel\Exception\HttpException;
 use Symfony\Component\Validator\Validator\ValidatorInterface;
 
-#[Route('/api', name:"api_")]
+#[Route('/api', name: "api_")]
 class ProductController extends AbstractController
 {
-    #[Route('/products', name: 'api_products', methods:'GET')]
+    #[Route('/products', name: 'api_products', methods: 'GET')]
     public function getAllProducts(ProductRepository $productRepository, SerializerInterface $serializer): JsonResponse
     {
-        if($this->isGranted("ROLE_ADMIN") === true){
+        if ($this->isGranted("ROLE_ADMIN") === true) {
             $products = $productRepository->findAll();
 
-            $jsonProducts = $serializer->serialize($products,'json');
+            $jsonProducts = $serializer->serialize($products, 'json');
 
-            return new JsonResponse($jsonProducts,Response::HTTP_OK,[],true);
+            return new JsonResponse($jsonProducts, Response::HTTP_OK, [], true);
         }
-        return throw new HttpException('403','You not authorized to see all products');
+        return throw new HttpException('403', 'You not authorized to see all products');
     }
 
-    #[Route('/products/{id}', name: 'api_product', methods:'GET')]
-    public function getOneProductById(int $id,ProductRepository $productRepository, SerializerInterface $serializer): JsonResponse
+    #[Route('/products/{id}', name: 'api_product', methods: 'GET')]
+    public function getOneProductById(int $id, ProductRepository $productRepository, SerializerInterface $serializer): JsonResponse
     {
-        $product = $productRepository->find($id);
+        if ($this->isGranted("ROLE_ADMIN") === true) {
+            $product = $productRepository->find($id);
 
-        if($product){
-            $jsonProduct = $serializer->serialize($product,'json');
-            return new JsonResponse($jsonProduct,Response::HTTP_OK,[],true);
+            if ($product) {
+                $jsonProduct = $serializer->serialize($product, 'json');
+                return new JsonResponse($jsonProduct, Response::HTTP_OK, [], true);
+            }
+            return throw new HttpException('404', "The ID doesn't exists");
         }
-        return new JsonResponse("This ID is not exist",Response::HTTP_NOT_FOUND);
-        
+        return throw new HttpException('403', 'You not authorized to see the product');
     }
 
-    #[Route('/products', name: 'api_product_create', methods:'POST')]
+    #[Route('/products', name: 'api_product_create', methods: 'POST')]
     public function createProduct(ValidatorInterface $validator, Request $request, ProductRepository $productRepository, SerializerInterface $serializer, EntityManagerInterface $entityManagerInterface): JsonResponse
     {
-        $product = $serializer->deserialize($request->getContent(),Product::class,'json');
+        if ($this->isGranted("ROLE_ADMIN") === true) {
+            $product = $serializer->deserialize($request->getContent(), Product::class, 'json');
 
-        $errors = $validator->validate($product);
+            $errors = $validator->validate($product);
 
-        if($errors->count() > 0){
-            return new JsonResponse($serializer->serialize($errors,'json'), JsonResponse::HTTP_BAD_REQUEST, [], 'true');
+            if ($errors->count() > 0) {
+                return new JsonResponse($serializer->serialize($errors, 'json'), JsonResponse::HTTP_BAD_REQUEST, [], 'true');
+            }
+
+            $entityManagerInterface->persist($product);
+            $entityManagerInterface->flush();
+
+            $jsonProduct = $serializer->serialize($product, 'json');
+
+            return new JsonResponse($jsonProduct, Response::HTTP_CREATED, [], true);
         }
-
-        $entityManagerInterface->persist($product);
-        $entityManagerInterface->flush();
-
-        $jsonProduct = $serializer->serialize($product,'json');
-
-        return new JsonResponse($jsonProduct,Response::HTTP_CREATED,[],true);
-        
+        return throw new HttpException('403', 'You not authorized to create a product');
     }
-
 }
